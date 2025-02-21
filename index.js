ccbd1b443332883ca8f6c04f1aa3628512dec2cb
Ayush Sharma <ayush200225@gmail.com>
2025-02-21 16:18:40 +0530

    Installed ejs and changed html file to ejs format and made changes to table

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import ProductController from './src/controllers/product.controller.js';
+import ProductController from './src/controller/product.controller.js';
+import path from 'path';
 
 const server = express();
 
-server.get('/');
+server.set("view engine", "ejs");
+server.set("views", path.join(path.resolve(), "src", "views"));
+
+const productController = new ProductController();
+server.get('/', productController.getProducts);
 
 server.use(express.static('src/views'))
 
--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,7 +1,12 @@
 import path from 'path';
+import ProductModel from '../module/product.model.js';
 
 export default class ProductController{
     getProducts(req, res){
-        res.sendFile(path.join(path.resolve(), "", "index.html"))
+        let products = ProductModel.get();
+        res.render("products", {products:products});
+        // return res.sendFile(
+        //     path.join(path.resolve(), "src", "views", "products.html")
+        // )
     }
 }
